aa2c8a71949106bd7ea130f7cc53beaf76b0d8d1
fabricio-joao <fabriciojoao41@outlook.com>
2023-02-14 07:03:20 -0300

    JavaEE-Implementação CRUD-Listar Contatos

--- src/main/java/com/agenda/servlet/AgendaControle.java
+++ src/main/java/com/agenda/servlet/AgendaControle.java
@@ -1,27 +1,54 @@
 package com.agenda.servlet;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.agenda.modelo.Agenda;
 import com.agenda.modelo.dao.AgendaDAO;
 import com.agenda.modelo.dao.FabricaDAO;
 
-@WebServlet("/agendacontrole")
+@WebServlet(urlPatterns = {"/agendacontrole", "/agenda"})
 public class AgendaControle extends HttpServlet {
+	
 	private static final long serialVersionUID = 1L;
+	
+	//conexão banco de dados
     AgendaDAO dao = FabricaDAO.criarAgendaDAO();  
+    
+    //instancia de objeto
+    Agenda contatos = new Agenda();
     
     public AgendaControle() {
         
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		System.out.println("METODO GET");
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		String url = request.getServletPath();
+		System.out.println(url);
+		
+		if(url.equals("/agenda")) {
+			listarContatos(request, response);
+		}
+	}
+	
+	protected void listarContatos(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		List<Agenda> listaContatos = dao.buscarLista();
+		for(Agenda contatos: listaContatos) {
+			System.out.println(contatos.getIdcon());
+			System.out.println(contatos.getNome());
+			System.out.println(contatos.getFone());
+			System.out.println(contatos.getEmail());
+		}
 	}
 
 }
